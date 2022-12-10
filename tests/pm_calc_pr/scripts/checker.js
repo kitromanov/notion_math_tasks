2f832c36d2da2ee8f900ceebc78f56a0a71be6ce
Kit <kitromanov@rambler.ru>
2022-12-10 21:17:13 +0300

    fix: answers variable scope

--- tests/pm_calc_pr/scripts/checker.js
+++ tests/pm_calc_pr/scripts/checker.js
@@ -1,101 +1,102 @@
 function checker(form, box_name) {
     var box = document.getElementById(box_name);
     switch(box_name) {
-        case 'box1':
-            if (form.answer1.value == 13) {
+        case 'box21':
+            console.log('sddddddd');
+            if (form.answer21.value == 13) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer1.value);
+                localStorage.setItem(box_name + "_answer", form.answer21.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box2':
-            if (form.answer2.value == 1023456789) {
+        case 'box22':
+            if (form.answer22.value == 1023456789) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer2.value);
+                localStorage.setItem(box_name + "_answer", form.answer22.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box3':
-            if (form.answer3.value == 285) {
+        case 'box23':
+            if (form.answer23.value == 285) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer3.value);
+                localStorage.setItem(box_name + "_answer", form.answer23.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box4':
-            if (form.answer4.value == 16) {
+        case 'box24':
+            if (form.answer24.value == 16) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer4.value);
+                localStorage.setItem(box_name + "_answer", form.answer24.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box5':
-            if (form.answer5.value == 7) {
+        case 'box25':
+            if (form.answer25.value == 7) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer5.value);
+                localStorage.setItem(box_name + "_answer", form.answer25.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box6':
-            if (form.answer6.value == 14208) {
+        case 'box26':
+            if (form.answer26.value == 14208) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer6 .value);
+                localStorage.setItem(box_name + "_answer", form.answer26 .value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box7':
-            if (form.answer7.value == 67200) {
+        case 'box27':
+            if (form.answer27.value == 67200) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer7 .value);
+                localStorage.setItem(box_name + "_answer", form.answer27 .value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box8':
-            if (form.answer8.value == 20) {
+        case 'box28':
+            if (form.answer28.value == 20) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer8.value);
+                localStorage.setItem(box_name + "_answer", form.answer28.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box9':
-            if (form.answer9.value == 22) {
+        case 'box29':
+            if (form.answer29.value == 22) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer9.value);
+                localStorage.setItem(box_name + "_answer", form.answer29.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box10':
-            if (form.answer10.value == 700) {
+        case 'box210':
+            if (form.answer210.value == 700) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer10.value);
+                localStorage.setItem(box_name + "_answer", form.answer210.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
             break;
-        case 'box11':
-            if (form.answer11.value == 22) {
+        case 'box211':
+            if (form.answer211.value == 22) {
                 box.style.background='rgb(161, 255, 21)';
                 localStorage.setItem(box_name, 'rgb(161, 255, 21)');
-                localStorage.setItem(box_name + "_answer", form.answer11.value);
+                localStorage.setItem(box_name + "_answer", form.answer211.value);
             } else {
                 localStorage.setItem(box_name + "_answer_flag", -1);
             }
